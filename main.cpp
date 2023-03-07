fed5fd47208b9120fc8a94710ca36dbb9af1735a
Musa Molla <musamolla3131@gmail.com>
2023-03-07 07:28:14 +0000

    It's working...

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using namespace std;
+
+class Animal {
+protected:
+  string name;
+
+public:
+  Animal(string name) : name(name) {}
+  void eat() { cout << name << "is eating." << endl; }
+};
+
+class Dog : public Animal {
+public:
+  Dog(string name) : Animal(name) {}
+  void bark() { cout << name << "is barking." << endl; }
+};
 
 int main() {
-  std::cout << "Hello World!\n";
+  Dog kutta("Buddy ");
+  kutta.eat();
+  kutta.bark();
 }
